2919de85c780867f51a251649fd876a72fee649f
sinedied <noda@free.fr>
2022-05-24 15:21:18 +0200

    Add finished code

--- www/app.js
+++ www/app.js
@@ -1,20 +1,35 @@
-const taskformElement = document.querySelector('#newTaskForm');
-const taskListElement = document.querySelector('#tasksList');
+const taskform = document.querySelector('#newTaskForm');
+const taskList = document.querySelector('#tasksList');
 const empty = document.querySelector('#empty');
-const usernameElement = document.querySelector('#username');
+const username = document.querySelector('#username');
+
+let nbTasks = 0;
 
 async function getUser() {
     // TODO
+    const response = await fetch('/.auth/me');
+    const payload = await response.json();
+
+    username.innerHTML = `Hi, ${payload.clientPrincipal.userDetails}`;
 }
 
-async function updateTask() {
-    // TODO
+async function updateTask(e) {
+    const taskId = this.closest('li').getAttribute('id');
+    const status = e.target.checked ? 'checked' : '';
+    
+    const response = await fetch(`/api/tasks/${taskId}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ status })
+    });
 }
 
-taskformElement.addEventListener('submit', async (e) => {
+taskform.addEventListener('submit', async (e) => {
 
     e.preventDefault();
-    const newTaskInput = taskformElement.elements.new_task_input;
+    const newTaskInput = taskform.elements.new_task_input;
 
     const response = await fetch('/api/tasks', {
         method: 'POST',
@@ -25,22 +40,28 @@ taskformElement.addEventListener('submit', async (e) => {
     });
     const payload = await response.json();
 
-    const task = generateTask(payload.task);
+    if (nbTasks === 0) {
+        document.querySelector("#empty").remove();
+    }
+    nbTasks++;
+    
+    const task = generateTask(payload);
 
-    taskListElement.appendChild(task);
+    taskList.appendChild(task);
 
     newTaskInput.value = '';
 });
 
 async function getTasks() {
     const response = await fetch('/api/tasks');
-    const tasks = await response.json();
-    console.log(tasks);
+    const payload = await response.json();
+    console.log(payload);
 
-    if (tasks) {
+    if (payload && payload.length > 0) {
+        nbTasks += payload.length;
         document.querySelector("#empty").remove();
-        for (const task of tasks) {
-            taskListElement.appendChild(generateTask(task))
+        for (const task of payload) {
+            taskList.appendChild(generateTask(task))
         }
     }
 }
@@ -58,7 +79,7 @@ function generateTask(task) {
     const fragment = range.createContextualFragment(tmpl);
 
     fragment.querySelector('input').addEventListener('change', updateTask);
-    
+
     return fragment;
 }
 
